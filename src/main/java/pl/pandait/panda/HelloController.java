4d15d74a4b3c498eebc8e82e61e35dcb4a242343
PandaAcademy <panda@pandait.com>
2021-01-31 17:40:18 +0000

    Local selenium tests

--- src/main/java/pl/pandait/panda/HelloController.java
+++ src/main/java/pl/pandait/panda/HelloController.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController{
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name",
-		required=false, defaultValue="World")
-		String name, Model model){
-		model.addAttribute("name", name);
-		return"greeting";
-	}
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting_template";
+    }
 }
 
 
