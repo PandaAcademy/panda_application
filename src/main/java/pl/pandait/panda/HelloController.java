a60650d88b7ff7f258c139b32b40b144a7f2c1eb
PandaAcademy <47747541+PandaAcademy@users.noreply.github.com>
2021-04-13 12:57:08 +0200

    Docker new (#4)
    docker

--- src/main/java/pl/pandait/panda/HelloController.java
+++ src/main/java/pl/pandait/panda/HelloController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController{
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name",
-		required=false, defaultValue="World")
-		String name, Model model){
-		model.addAttribute("name", name);
-		return "greeting_template";
-	}
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting_template";
+    }
+
 }
 
 
